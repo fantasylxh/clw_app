47a61270e130ab212ee77d685623a73580c8ba49
lostofland <772932587@qq.com>
2017-05-25 17:01:56 +0800

    ａｄｄｉｇｎｏｒｅ

--- public/shop/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('SYSTEM_IN') or exit('Access Denied');
-$BJCMS_CONFIG = array();
-$BJCMS_CONFIG['db']['host'] = 'localhost';
-$BJCMS_CONFIG['db']['username'] = 'root';
-$BJCMS_CONFIG['db']['password'] = 'root';
-$BJCMS_CONFIG['db']['port'] = '3306';
-$BJCMS_CONFIG['db']['database'] = 'wx_app';
