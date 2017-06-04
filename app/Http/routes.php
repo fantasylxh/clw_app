a2a9492cbaa959bc3cd8e1340984b36189e12320
lostofland <772932587@qq.com>
2017-06-04 17:14:52 +0800

    积分查询

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,6 +31,7 @@ Route::group(['middleware' => [], 'namespace' => 'Api'], function () {
     Route::group(['prefix' => 'user', 'namespace' => 'User'], function () {
         Route::any('/get-wx-user-info', 'UserController@getWxUserInfo');
         Route::post('/store', 'UserController@store');//注册
+        Route::post('/credit', 'UserController@credit');//积分查询
     });
 
     Route::group(['prefix' => 'product', 'namespace' => 'Product'], function () {
@@ -39,53 +40,16 @@ Route::group(['middleware' => [], 'namespace' => 'Api'], function () {
 
     });
 
-    Route::group(['prefix' => 'supplier', 'namespace' => 'Supplier'], function () {
-        Route::get('/', 'SupplierController@index');
-        Route::get('/detail', 'SupplierController@detail');
-        Route::any('/follow', 'SupplierController@follow');
-        Route::any('/unfollow ', 'SupplierController@unfollow');
-    });
-
     Route::group(['prefix' => 'personal', 'namespace' => 'Personal'], function () {
         Route::get('/', 'PersonalController@index');
         Route::get('/order-list', 'PersonalController@orderList');
         Route::get('/collection-list', 'PersonalController@collectionList');
         Route::get('/attention-list', 'PersonalController@attentionList');
     });
-    Route::group(['prefix' => 'apply', 'namespace' => 'Apply'], function () {
-        Route::any('/', 'ApplyController@index');
-        Route::get('/success', 'ApplyController@success');
-    });
 
     Route::group(['prefix' => 'article', 'namespace' => 'Article'], function () {
         Route::get('/', 'ArticleController@index');
     });
 
-    Route::get('/about-us', function () {
-        return view('about-us');
-    });
-    Route::get('/unfinished', function () {
-        return view('unfinished');
-    });
-    Route::get('/video', function () {
-        return view('video', ['videos' => \App\Models\ProductVideo::all()]);
-    });
-
-    Route::get('/mime-video', function () {
-        return view('video-for-mime');
-    });
-
-    Route::get('/phone', function () {
-        return view('phone');
-    });
-
-    Route::group(['prefix' => 'test'], function () {
-        Route::get('/success', 'TestController@success');
-    });
 });
 
-
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
-    Route::get('/export', 'TestController@export');
-    Route::get('/test', 'TestController@test');
-});
