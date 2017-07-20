c434d7f42b28b2e8349ad7ae7cfcab5fba75b81e
lostofland <772932587@qq.com>
2017-07-20 22:20:47 +0800

    分类

--- public/shop/system/article/template/web/article.php
+++ public/shop/system/article/template/web/article.php
@@ -146,40 +146,7 @@
    
    
 	<div class='row'>
-		
-		<div class="col-sm-5" style='padding-right:1px;padding-left:1px;'>
-		
-		
-		<div class="fart-preview">
-        <div class="top"><p bind-to="art_title"><?php  if($aid) { ?><?php  echo $article['article_title'];?><?php  } else { ?>这里是文章标题<?php  } ?></p></div>
-        <div class="main">
-            <div class="fart-rich-primary">
-                <div class="fart-rich-title" bind-to="art_title"><?php  if($aid) { ?><?php  echo $article['article_title'];?><?php  } else { ?>这里是文章标题<?php  } ?></div>
-                <div class="fart-rich-mate">
-                    <div class="fart-rich-mate-text" bind-to="art_date_v"><?php  if($aid) { ?><?php  echo $article['article_date_v'];?><?php  } else { ?><?php  echo date('Y-m-d');?><?php  } ?></div>
-                    <div class="fart-rich-mate-text" bind-to="art_author"><?php  if($aid) { ?><?php  echo $article['article_author'];?><?php  } else { ?>编辑小美<?php  } ?></div>
-                    <div class="fart-rich-mate-text href" bind-to="art_mp"><?php  if(empty($article['article_mp'])) { ?><?php  echo $mp['name'];?><?php  } else { ?><?php  echo $article['article_mp'];?><?php  } ?></div>
-                </div>
-                <div class="fart-rich-content" id="preview-content">
-                    <?php  echo htmlspecialchars_decode($article['article_content'])?>
-                </div>
-                <div class="fart-rich-tool">
-                    <div class="fart-rich-tool-text link">查看原文</div>
-                    <div class="fart-rich-tool-text"><i class="icon icon-person2"></i></div>
-                    <div class="fart-rich-tool-text" bind-to="art_read"> <?php  if($aid) { ?><?php  if($article['article_readnum_v']>100000) { ?>100000+<?php  } else { ?><?php  echo $article['article_readnum_v'];?><?php  } ?><?php  } else { ?>100000+<?php  } ?></div>
-                    <div class="fart-rich-tool-text"><i class="icon icon-likefill text-danger"></i></div>
-                    <div class="fart-rich-tool-text">
-                        <span bind-to="art_like"> <?php  if($aid) { ?><?php  if($article['article_likenum_v']>100000) { ?>100000+<?php  } else { ?><?php  echo $article['article_likenum_v'];?><?php  } ?><?php  } else { ?>54321<?php  } ?></span>
-                    </div>
-                    <div class="fart-rich-tool-text right">反馈</div>
-                </div>
-            </div>
-           
-        </div>
-        <!-- 手机 -->
-    </div>
-		
-			</div>
+
 
 		<div class="col-sm-7" style='padding-right:1px;padding-left:1px;position:relative'>
 	
@@ -215,15 +182,17 @@
                       
                     </div>
 
-                    <div class="col-sm-8 col-xs-12">
-
-
-                        <?php  echo tpl_form_field_category_2level('category', $parent, $children, $item['pcate'], $item['ccate'])?>
-
-                    </div>
                 </div>
-                
-					
+
+                        <div class="form-group">
+                            <label class="col-xs-12 col-sm-3 col-md-2 control-label">区域分类</label>
+                            <div class="col-sm-8 col-xs-12">
+
+
+                                <?php  echo tpl_form_field_category_2level('category', $parent, $children, $article['pcate'], $article['ccate'])?>
+
+                            </div>
+                        </div>
 					
 					  <div class="form-group">
                  	<label class="col-sm-2 control-label"></label>
